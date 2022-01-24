c2fc738386934278fa2a8b658c139750fe7f2463
anukrati1507 <47574465+anukrati1507@users.noreply.github.com>
2022-01-24 11:24:53 +0530

    Update hello.go

--- hello.go
+++ hello.go
@@ -2,43 +2,38 @@ package main
 
 import (
 	"time"
-	"fmt"
 )
 
 type Data struct {
-	data int
-	expTime int
+	data    int
+	expTime int64
 }
-
 type InMap struct {
-	Map map[string]Value
+	Map map[string]Data
+}
+type Parameters struct {
+	data int
+	key  string
+	ttl  int64
 }
 
-type Param struct {
-	data int,
-	key string
-	ttl int
-}
-
-func (c *InMap) set(param Param) {
-	if param.ttl == nil {
-		param.ttl=60
-	} else if param.ttl<0 {
+func (c *InMap) set(param Parameters) {
+	if param.ttl == 0 {
+		param.ttl = 60
+	} else if param.ttl < 0 {
 		panic("Time cannot be a negative entity!")
 	}
-	c.Map[param.key] = Data{data: param.data, expTime: time.Now().Unix()+param.ttl}
+	c.Map[param.key] = Data{data: param.data, expTime: time.Now().Unix() + param.ttl}
 }
-
-func (c *InMap) get(key String) {
+func (c *InMap) get(key string) (value int) {
 	var state bool
 	var data1 Data
 	data1, state = c.Map[key]
-	value = item.data
+	value = data1.data
 	if !state {
 		panic("Key is not found in the cache.")
 	}
 	return
-	
 }
 
 /*func get(key int) {
@@ -49,31 +44,28 @@ func (c *InMap) get(key String) {
 		fmt.println("Key not found!")
 	}
 }
-
 func set(key int,value int,time int) {
 	if(time==nil){
 		time=60
 	}
 }*/
-func (c *InMap) delete(key String) {
+func (c *InMap) delete(key string) {
 	var state bool
 	_, state = c.Map[key]
 	if !state {
-		panic("ERR: Key not found.")
+		panic("Error, Key not found in the map.")
 	}
 	delete(c.Map, key)
 }
-
 func newCache() (inmap InMap) {
 	inmap = InMap{Map: make(map[string]Data)}
 	return
 }
-
-func checkExpiry(inmap *InMap){
+func checkExpiry(inmap *InMap) {
 	for {
 		for key, value := range inmap.Map {
-			if value.expirationTime < time.Now().Unix() {
-				delete(inmap.Map,key)
+			if value.expTime < time.Now().Unix() {
+				delete(inmap.Map, key)
 			}
 		}
 	}
